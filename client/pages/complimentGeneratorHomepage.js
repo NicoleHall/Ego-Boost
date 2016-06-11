f83b18a1366828fa6193b6f516bc1e2a498e3b81
Nicole Hall <nicolealexandrahall@gmail.com>
2016-06-11 15:18:22 -0600

    two compliments are now inside the complimentsArray. Hooray

--- client/pages/complimentGeneratorHomepage.js
+++ client/pages/complimentGeneratorHomepage.js
@@ -6,3 +6,8 @@ require('bootstrap');
 
 import _ from 'underscore';
 import Backbone from 'backbone';
+
+var complimentArray = [
+  "You are the only person I trust to work on my Harley",
+  "The Beatle's song 'Here Comes The Sun' reminds me of you",
+]
