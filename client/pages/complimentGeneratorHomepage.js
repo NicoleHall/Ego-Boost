680e556e0a44017323bcf6ef7086764301b2a0b9
Nicole Hall <nicolealexandrahall@gmail.com>
2016-06-16 15:06:58 -0600

    set up router file, database file and compliemnt generator view

--- client/pages/complimentGeneratorHomepage.js
+++ client/pages/complimentGeneratorHomepage.js
@@ -7,7 +7,11 @@ require('bootstrap');
 import _ from 'underscore';
 import Backbone from 'backbone';
 
-var complimentArray = [
-  "You are the only person I trust to work on my Harley",
-  "The Beatle's song 'Here Comes The Sun' reminds me of you",
-]
+var app = {
+  init: function(){
+    var complimentGenerator = Backbone.View.extend({})
+  };
+}
+
+
+module.exports = app;
--- server/routes.js
+++ server/routes.js
@@ -3,14 +3,29 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var fs = require('fs');
 
+// compliment route
+
+var databasePath = __dirname + '/database.json';
+
+router.get('/compliment', function(req, res){
+  fs.readFile(databasePath, function(err, data){
+
+    if (err) { console.log(err); }
+
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.write(data);
+    res.end();
+  });
+});
+
+//  everything route
 router.get('/*', function indexRouteHandler (req, res) {
   res.render('view', {
   	title: "Website Example",
     token: _.uniqueId()
   });
 });
-
-// TODO: API routes
 
 module.exports = router;
