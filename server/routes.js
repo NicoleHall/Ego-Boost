9850096f17c548e2567e8efa81be3980457cc990
Nicole Hall <nicolealexandrahall@gmail.com>
2016-06-28 17:45:53 -0600

    added some styling to make the compliment page look more like the landing page

--- server/routes.js
+++ server/routes.js
@@ -13,11 +13,28 @@ router.get('/compliment', function(req, res){
   fs.readFile(databasePath, function(err, data){
 
     if (err) { console.log(err); }
-
+    data = JSON.parse(data.toString('utf8'));
+    var complimentArray = data["complimentArray"];
+    var randomCompliment = complimentArray[Math.floor(Math.random()*complimentArray.length)];
     res.writeHead(200, {'Content-Type': 'text/json'});
-    res.write(data);
+    res.write(JSON.stringify(randomCompliment));
     res.end();
   });
+});
+
+router.post('/compliment', function(req, res){
+  var compliments = req.body;
+  console.log(compliments);
+  var completeFile;
+  fs.readFile(databasePath, function(err, data){
+    if (err) { console.log(err); }
+    completeFile = JSON.parse(data.toString('utf8'));
+  });
+  completeFile["complimentArray"].push(data);
+  // want to get the body of the post into the complimentArray
+  // stingify the compliments file
+  // save it into the completeFile
+  fs.writeFile(databasePath, completeFile);
 });
 
 //  everything route
